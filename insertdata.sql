2c9b2e52b235df666ea78f66e0644990b909203d
huiwengoh <gohhuiwen1999@gmail.com>
2020-12-06 12:07:39 -0500

    added data dump

---
+++ insertdata.sql
@@ -0,0 +1,12 @@
+insert into member (name, email, phone_num) values ('John Smith', 'jsmith@gmail.com', 3847275692);
+insert into member (name, email, phone_num) values ('David Gold', 'dgold@gmail.com', 2614804811);
+insert into member (name, email, phone_num) values ('Jane Doe', 'jdoe@gmail.com', 7913725593);
+insert into member (name, email, phone_num) values ('Paul Kim', 'pkim@gmail.com', 9125837353);
+insert into member (name, email, phone_num) values ('Edward Jones', 'ejones@gmail.com', 5729471137);
+insert into member (name, email, phone_num) values ('Lindsay Brown', 'lbrown@gmail.com', 7562957265);
+insert into member (name, email, phone_num) values ('Juan Carlos', 'jcarlos@gmail.com', 9824725775);
+insert into member (name, email, phone_num) values ('Maria Lopes', 'mlopez@gmail.com', 6729475628);
+insert into member (name, email, phone_num) values ('Daniel Lee', 'dlee@gmail.com', 2950184672);
+insert into member (name, email, phone_num) values ('Mark Williams', 'mwilliams@gmail.com', 3759174590);
+
+insert into book_info (ISBN, title, author, edition, year_published, publisher, genre, pages) values ();
