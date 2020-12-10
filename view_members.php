611b16d5497312943c86173e1b3dfdcf3e6aba61
huiwengoh <gohhuiwen1999@gmail.com>
2020-12-10 14:15:11 -0500

    added borrowed list to view members page

--- view_members.php
+++ view_members.php
@@ -32,17 +32,32 @@
 					<td> Name </td> 
 					<td> E-mail </td> 
 					<td> Phone Number </td>
+					<td> Currently Borrowed Books </td>
 				</tr>';
 	  while($row = $result->fetch_assoc()) {
 	    //echo "MemberID: " . $row["memberID"]. "<br>Name: " . $row["name"]. "<br> E-mail: " . $row["email"]. "<br> Phone Number:" . $row["phone_num"]. "<br><br>";
+
 			echo '<tr> 
 						  <td>'.$row["memberID"].'</td> 
 						  <td>'.$row["name"].'</td> 
 						  <td>'.$row["email"].'</td> 
 						  <td>'.$row["phone_num"].'</td> 
-						  
-					  </tr>';	
+				';
 
+			$memberid = $row["memberID"];
+			$books = "SELECT borrows.bookID, title FROM borrows, book_copy, book_info WHERE borrows.memberID = '$memberid' AND borrows.bookID = book_copy.bookID AND book_copy.ISBN = book_info.ISBN";
+			$book_res = $conn->query($books);
+
+			echo '<td>';
+			if ($book_res->num_rows == 0) {
+				echo 'N/A';
+			} else {
+				while($book_row = $book_res->fetch_assoc()) {
+					echo 'ID' . $book_row['bookID'] . ': ' . $book_row['title'] . '<br>';
+				}
+			}
+			echo '</td>';  
+			echo '</tr>';	
 		}
 		echo '</table>';
 	} else {
